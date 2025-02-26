576e88e264f8090ab13b50a648686f3f03c12ca1
= <=>
2025-02-26 13:15:25 -0400

    Añadiendo el carrusel de imágenes, la tabla desplegable en el encabezado y las banderas del carrusel

---
+++ XDXD.js
@@ -0,0 +1,141 @@
+let imagenes = [
+    {
+        "url": "Carrusel de imágenes/Bandera_de_Fiñana.svg.png",
+        "nombre": "Fiñana",
+        "descripcion": "Fiñana se encuentra en la provincia de Almería. En el año 2021 contaba con 1977 habitantes (INE). Su término municipal tiene una superficie de 133,85 km² lo que proporciona una densidad poblacional de 14,77 hab/km². Fiñana fue una frontera en disputa, conformando así un crisol de culturas. Una Fortaleza Mora, una iglesia y una mezquita son sus hitos."
+    },
+    {
+        "url": "Carrusel de imágenes/Flag_of_Dólar_Spain.svg.png",
+        "nombre": "Dólar",
+        "descripcion": "Está ubicado en la comarca de Guadix, provincia de Granada. Existen tres teorías sobre el origen del nombre. La primera es que, según el historiador nazarí Ibn al-Jatib, el nombre hace referencia a los maestros toneleros que trabajaban con hachas conocidas como dólar, los cuales se asentaron en el pueblo. Esta teoría data del siglo XIV, cuando ya se conocía al pueblo como Dólar. También puede deberse a Dolaria, término alusivo a un lugar abundante en madera, en relación con un bosque primitivo muy cercano a la localidad. Por último, el topónimo puede derivar del árabe Dār («[la] casa»), lo que puede indicar que ahí estuvo una posada o fonda, camino al puerto de La Ragua. Tiene 78,52 km2 de superficie, y unos 606 habitantes para 2024",
+    },
+    {
+        "url": "Carrusel de imágenes/Bandera_de_Palomares_del_Río_(Sevilla).svg.png",
+        "nombre": "Palomares del Río",
+        "descripcion": "Se ubica en la provincia de Sevilla. El término municipal, ubicado en la comarca del Aljarafe, tiene una población de 9277 habitantes (INE 2024). Forma parte del área metropolitana de Sevilla. Tiene 13,13 km2 de superficie",
+    },
+    {
+        "url": "Carrusel de imágenes/Bandera_de_Medina_de_Rioseco.svg.png",
+        "nombre": "Medina de Rioseco",
+        "descripcion": "Se localiza en la provincia de Valladolid. Los vestigios más antiguos de Rioseco corresponden a la Edad del Hierro y la época celtibérica. Cuenta con 4537 habitantes y 115,17 km2 de extensión. Perteneciente a la comarca de Tierra de Campos, posee los títulos honoríficos de muy noble y muy leal, así como el de ciudad, y es conocida también por los sobrenombres de la Ciudad de los Almirantes y la Vieja India Chica",
+    },
+    {
+        "url": "Carrusel de imágenes/Bandera_de_San_Sebastián_de_La_Gomera.png",
+        "nombre": "San Sebastián de La Gomera",
+        "descripcion": "Es la capital de la isla de La Gomera, en Canarias. Cuenta con 9342 habitantes y una superficie de 113,59 km2. El puerto de San Sebastián fue la última parada de la primera expedición de Cristóbal Colón antes de alcanzar el continente americano.",
+    },
+    {
+        "url": "Carrusel de imágenes/Bandera_de_Tahal.svg.png",
+        "nombre": "Tahal",
+        "descripcion": "Ubicado en la provincia de Almería, cuenta con 375 habitantes y una superficie de 94,53 km2. Al nombre de Tahal se le supone una procedencia amazig, con un significado de barranco húmedo.",
+    },
+    {
+        "url": "Carrusel de imágenes/Bandera_de_Valdeverdeja.svg.png",
+        "nombre": "Valdeverdeja",
+        "descripcion": "Se encuentra en la provincia de Toledo. El término municipal, ubicado en la comarca de la Campana de Oropesa, tiene una población de 565 habitantes (INE 2024). Y cuenta con 67 km2 de extensión. Asentado en el valle del Tajo sobre un terreno granítico que forma las llamados canchales. El Tajo discurre encajonado con orillas escarpadas. En sus márgenes hay, ya en desuso, varios molinos de agua. La vegetación, compuesta de dehesas pobladas de encinas y de olivos y de monte bajo con retama y tomillo es típicamente mediterránea.",
+    },
+    {
+        "url": "Carrusel de imágenes/Flag_of_Puebla_de_Don_Rodrigo_Spain.svg.png",
+        "nombre": "Puebla de Don Rodrigo",
+        "descripcion": "Se encuentra en la provincia de Ciudad Real. Cuenta con 1137 habitantes repartidos en 424,87 km2. Debe su nombre a Don Rodrigo Téllez de Girón, vigésimo noveno maestre de la Orden de Calatrava durante el año 1472, quien la convirtió en villa. Por este motivo sus habitantes le concedieron su nombre. Hay constancia de poblamiento desde época prerromana, tal y como lo demuestra la presencia de varios castellares, como el del Castillo o el de Castilnegro, o los hallazgos encontrados junto al pueblo a partir del año 1970. En época musulmana fue aldea de Piedrabuena.",
+    },
+    {
+        "url": "Carrusel de imágenes/Bandera_de_Mérida.svg.png",
+        "nombre": "Mérida",
+        "descripcion": "Situado en la provincia de Badajoz, es la capital de Extremadura. Tiene 59857 habitantes y 	865,19 km². según la tradición historiográfica, fue fundada como colonia romana en el año 25 a. C. por orden del emperador Octavio Augusto para servir de retiro a los soldados veteranos (eméritos) de las legiones V Alaudae y X Gemina; de ahí su nombre romano de Emerita Augusta.",
+    },
+    {
+        "url": "Carrusel de imágenes/Bandera_de_Banyeres_de_Mariola.svg.png",
+        "nombre": "Banyeres de Mariola",
+        "descripcion": "También conocido como Bañeres, es un municipio de la Comunidad Valenciana, en la provincia de Alincante. Tiene una población de 7307 habitantes y 50,28 km². Se cree que el topónimo es de origen árabe, aunque lógicamente a partir de otro nombre anterior. Se especula con la denominación Beni-Hares -hijo de liebres- aunque, según Escolano, el topónimo es anterior y los árabes lo habrían llamado Bernirehes.",
+    },
+    {
+        "url": "Carrusel de imágenes/Bandera_de_Buitrago_del_Lozoya.svg.png",
+        "nombre": "Buitrago del Lozoya",
+        "descripcion": "Se ubica al norte de la Comunidad de Madrid, situada a los pies de la sierra de Guadarrama. El municipio, que cuenta con una población de 1973 habitantes (INE 2024) y 26,5 km2, recibe su nombre del río Lozoya, que circunda la localidad. Destaca por la conservación de su antiguo recinto amurallado y su casco urbano está declarado bien de interés cultural.",
+    },
+    {
+        "url": "Carrusel de imágenes/Bandera_de_La_Torre_de_Esteban_Hambrán.svg.png",
+        "nombre": "La Torre de Esteban Hambrán",
+        "descripcion": "Se ubica en la provincia de Toledo. Tiene 1932 habitantes y 51 km2. El topónimo podría derivarse de una antigua torre musulmana, utilizada para comunicarse por medio de fuegos con el castillo de Alamín, que pudo existir en el siglo XIII, donde hoy se encuentra la villa, y que pertenecería al mozárabe Esteban Ambrán.",
+    },
+    {
+        "url": "Carrusel de imágenes/Bandera_de_San_Martín_de_la_Vega.svg.png",
+        "nombre": "San Martín de la Vega",
+        "descripcion": "Se encuentra al sur de la comunidad de Madrid. Tiene 20733 habitantes y 105,93 km2. Más del 80 % de su término municipal está incluido en el ámbito del parque regional del Sureste, dado su alto interés ecológico y medioambiental. Asimismo, se ha situado como un enclave turístico debido a la apertura en 2002 de Parque Warner, el parque temático más grande e importante del mundo de los dedicados a la compañía Warner Bros.",
+    },
+    {
+        "url": "Carrusel de imágenes/Bandera_de_Trujillanos.svg.png",
+        "nombre": "Trujillanos",
+        "descripcion": "Ubicado en la provincia de Badajoz, se llama así por el hecho de que sus primeros pobladores provenían de Trujillo. Cuenta con 1382 habitantes y 20,3 km2",
+    },
+    {
+        "url": "Carrusel de imágenes/Bandera_de_Vilches_(Jaén).svg.png",
+        "nombre": "Vilches",
+        "descripcion": "Se localiza en la provincia de Jaén. Cuenta con 4190 habitantes y 272,92 km2. Es el pueblo con más kilómetros de costas interiores de toda España, con tres pantanos dentro de su término: el de embalse de La Fernandina, el pantano del Guadalén y el más reciente, el embalse de Giribaile.",
+    },
+    {
+        "url": "Carrusel de imágenes/bandera-trujillo-españa.png",
+        "nombre": "Trujillo",
+        "descripcion": "Se ubica en la provincia de Cáceres. Tiene 8619 habitantes y 649,53 km². Su importancia histórica, su situación, su entorno natural y su gastronomía han hecho de Trujillo un importante centro turístico de Extremadura. La ciudad de Trujillo ha recibido los nombres de Turaca/Turacia, Turgalium, Torgiela, Troxiello, Trugillo y Trujillo a lo largo de la Historia. Según varios estudios, Turaca o Turacia era el nombre prerromano de la ciudad. Posteriormente, en la época romana se llamó con la voz latina Turgalium. En la evolución de Turgalium al actual topónimo de Trujillo debió haber una influencia árabe o mozárabe, pues de lo contrario la evolución natural de la palabra hubiera dado lugar a Trugajo o Trugallo en lugar de Trujillo. Trujillo además da nombre a varias localidades de América, fundadas por personas originarias de la ciudad española como Trujillo (Honduras) (1525), Trujillo (Perú) (1535), Trujillo (Venezuela) (1556), Trujillo Alto (Puerto Rico) (1801) y Trujillo (Colombia) (1922).",
+    },
+    {
+        "url": "Carrusel de imágenes/BanderaPalosEscudo0.svg.png",
+        "nombre": "Palos de la Frontera",
+        "descripcion": "Localizado en la provincia de Huelva, cuenta con 12663 habitantes y 50 km2. La historia de esta ciudad está íntimamente ligada a las labores marítimas y a los descubrimientos geográficos. Es por lo que Palos de la Frontera es conocida como la cuna del Descubrimiento de América (como afirma en su escudo), ya que en esta ciudad se gestó y se preparó el primer viaje de Cristóbal Colón hacia las Indias. Zarparon del puerto de esta ciudad el 3 de agosto de 1492, llegando el 12 de octubre de dicho año a una isla del actual continente americano, que por entonces era desconocido por los europeos.",
+    },
+    {
+        "url": "Carrusel de imágenes/Flag_of_Granja_de_Rocamora.svg.png",
+        "nombre": "Granja de Rocamora",
+        "descripcion": "Situado al sur de la provincia de Alicante, cuenta con 2739 habitantes y 7,2 km2. De origen musulmán, Granja de Rocamora pudo formarse a partir de una antigua alquería. Estuvo bajo el dominio de la familia Rocamora, antepasados y primeros condes de Granja de Rocamora, que poseían también las heredades de Benferri y Puebla de Rocamora desde que el 25 de agosto de 1265 las adquirieran en el reparto hecho por Alfonso X el Sabio en Córdoba.",
+    },
+]
+
+let atras = document.getElementById('atras');
+let adelante = document.getElementById('adelante');
+let imagen = document.getElementById('img');
+let texto = document.getElementById('texto')
+let actual = 0
+let puntos = document.getElementById('puntos');
+posicionCarrusel()
+
+atras.addEventListener('click', function(){
+    actual -=1
+
+    if (actual == -1){
+        actual = imagenes.length - 1
+    }
+
+    imagen.innerHTML = ` <img class="img" src="${imagenes[actual].url}" alt="logo pagina" loading="lazy"></img>`
+    texto.innerHTML = `
+    <h3>${imagenes[actual].nombre}</h3>
+    <p>${imagenes[actual].descripcion}</p>
+    `
+    posicionCarrusel()
+})  
+adelante.addEventListener('click', function(){
+    actual +=1
+
+    if (actual == imagenes.length){
+        actual = 0
+    }
+
+    imagen.innerHTML = ` <img class="img" src="${imagenes[actual].url}" alt="logo pagina" loading="lazy"></img>`
+    texto.innerHTML = `
+    <h3>${imagenes[actual].nombre}</h3>
+    <p>${imagenes[actual].descripcion}</p>
+    `
+    posicionCarrusel()
+})  
+
+function posicionCarrusel() {
+    puntos.innerHTML = ""
+    for (var i = 0; i <imagenes.length; i++){
+        if(i == actual){
+            puntos.innerHTML += '<p class="bold">.<p>'
+        }
+        else{
+            puntos.innerHTML += '<p>.<p>'
+        }
+    } 
+}
